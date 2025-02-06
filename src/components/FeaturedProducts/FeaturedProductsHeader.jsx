8f4fc17fa59c89d0ced9f02cdef015dc352d3d7e
edogola4 <edogola4@gmail.com>
2025-02-06 18:41:49 +0300

    updated

---
+++ src/components/FeaturedProducts/FeaturedProductsHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FeaturedProductsHeader = () => {
+  return (
+    <div className="title">
+      <h2>Featured Products</h2>
+      <div className="underline" />
+    </div>
+  );
+};
+
+export default FeaturedProductsHeader;
