951e7e8e3545372a115a196298a6dccc6e7edcff
edogola4 <edogola4@gmail.com>
2025-02-06 16:02:25 +0300

    convert component to JavaScript and add breadcrumb navigation

---
+++ src/components/PageHero.js
@@ -0,0 +1,43 @@
+// src/components/PageHero.js
+// --------------------------------------------------------------------
+// PageHero Component
+// This component displays a breadcrumb navigation for the current page,
+// showing links to 'Home' and 'Products' (if applicable), followed by
+// the current page title.
+// --------------------------------------------------------------------
+
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const PageHero = ({ title, isSingleProduct }) => {
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>
+          <Link to="/">Home</Link>
+          {isSingleProduct && <Link to="/products">/ Products</Link>}/ {title}
+        </h3>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  background: var(--clr-primary-10);
+  width: 100%;
+  min-height: 20vh;
+  display: flex;
+  align-items: center;
+  color: var(--clr-primary-1);
+  a {
+    color: var(--clr-primary-3);
+    padding: 0.5rem;
+    transition: var(--transition);
+  }
+  a:hover {
+    color: var(--clr-primary-1);
+  }
+`;
+
+export default PageHero;
